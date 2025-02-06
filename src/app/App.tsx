c0097474947799c996b09d9c5bac0a2a438b70e2
Степан Кучеренко <s.kucherenko.pirit@yandex.ru>
2025-02-06 17:13:54 +0700

    Configure redux

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { TodoList } from "../module/todo-list/todo-list";
 import { queryClient } from "../shared/api/query-client";
+import { Provider } from "react-redux";
+import { store } from "../shared/redux";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TodoList />
+      <Provider store={store}>
+        <TodoList />
+      </Provider>
     </QueryClientProvider>
   );
 }
